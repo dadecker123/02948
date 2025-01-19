4c0045ab05accda86f24cc69488c54b5be22021f
Drake D <drakedecker2020@gmail.com>
2025-01-19 13:47:31 -0500

    Main

---
+++ bin/public class Main {.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(string args[]) {
+        system.out.println("Welcome to Java!");
+        system.out.println("Welcome to computer science.");
+        system.out.println("Programming is fun.");
+    }
+}
