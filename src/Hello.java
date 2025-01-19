7c37e90d93de28abeed8d289e62eaaf332573ce0
Drake D <drakedecker2020@gmail.com>
2025-01-19 14:53:30 -0500

    Modified string name in Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -1,9 +1,9 @@
 public class Hello {
     public static void main(String args[]) {
-        String str = "Hello World!";
+        String first = "Hello World!";
 
         for (int i = 0; i < 5; i++) {
-            System.out.println(str);
+            System.out.println(first);
         }
     }
 }
