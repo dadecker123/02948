a8e26c43b0b40905f494526e035537fbdb698eee
Drake D <drakedecker2020@gmail.com>
2025-01-19 14:07:35 -0500

    created file to print message

--- bin/public class Main {.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(string args[]) {
-        system.out.println("Welcome to Java!");
-        system.out.println("Welcome to computer science.");
-        system.out.println("Programming is fun.");
-    }
-}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Welcome to Java!");
+        System.out.println("Welcome to computer science.");
+        System.out.println("Programming is fun.");
+    }
+
+    @Override
+    public String toString() {
+        return "Main []";
+    }
+}
