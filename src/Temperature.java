18616a58fa18533f32c3adc22df0434814917ad8
Drake D <drakedecker2020@gmail.com>
2025-01-30 20:28:58 -0500

    Created program to convert temperatures

---
+++ src/Temperature.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Temperature {
+    public static void main(String args[]) {
+        //asks user for celsius value
+        System.out.println("Please enter your temperature in Celsius:" );
+        //Instantiates scanner
+        Scanner input = new Scanner(System.in);
+        //Retrieves value from input
+        double celsius = input.nextDouble();
+        //Establishes fahrenehit variable
+        double fahrenheit = 0;
+        //performs celsius to fahrenheit calculation
+        fahrenheit = (9.0 / 5) * celsius + 32;
+        //print value 
+        System.out.println("Your temperature in Fahrenehit is: " + fahrenheit); 
+    }
+}
