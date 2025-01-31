1ed2e7bf0c71e1de54d9d555b15b6a0adbb3bb0f
Drake D <drakedecker2020@gmail.com>
2025-01-30 20:43:54 -0500

    Created program to calculate total cost of meal

---
+++ src/Gratuity.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Gratuity {
+    public static void main(String args[]) {
+        System.out.println("Please enter your subtotal: ");
+            Scanner input = new Scanner(System.in);
+            double subTotal = input.nextDouble();
+        System.out.println("Please enter percent gratuity: ");
+            double gratuity = input.nextDouble();
+            double tip = subTotal * (gratuity / 100);
+            double total = subTotal + tip;
+            System.out.println("Your total is: " + total + " dollars.");
+
+
+
+
+    }
+}
