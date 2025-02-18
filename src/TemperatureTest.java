2ce35c6ac80a95f9e43203dce4f1ac8cf9412e90
Drake D <drakedecker2020@gmail.com>
2025-02-17 21:38:52 -0500

    bugs

--- src/TemperatureTest.java
+++ src/TemperatureTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 public class TemperatureTest {
     @Test
     public void testMain() {
-        Temperature temperature = new Temperature();
         double celsius = 37;
         double actualResult = (9.0 / 5) * celsius + 32;
         Assert.assertEquals(98.60000000000001,actualResult , 0);
