f0e60b463fc36f7313ee8b996432ec8be61c9f72
Drake D <drakedecker2020@gmail.com>
2025-03-19 14:59:11 -0400

    Created display to show miles/kilometers conv.

---
+++ src/MilesToKilometers.java
@@ -0,0 +1,8 @@
+public class MilesToKilometers {
+    public static void main(String args[]) {
+        System.out.println("Miles:  Kilometers:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + "       " + (i*1.609));
+        }
+    }
+}
