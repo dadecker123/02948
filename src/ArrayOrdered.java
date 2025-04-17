ec7da3cdbe2e429b0f81247cfd23f7270bc1483f
Drake D <drakedecker2020@gmail.com>
2025-04-16 20:52:08 -0400

    syntax

--- src/ArrayOrdered.java
+++ src/ArrayOrdered.java
@@ -4,7 +4,7 @@ public class ArrayOrdered {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         double array[] = new double[10];
-        System.out.println("Enter ten numbers for adding.");
+        System.out.println("Enter ten numbers for averaging.");
         for (int i = 0;i < array.length;i++){
             array[i] = scanner.nextDouble();
         }
