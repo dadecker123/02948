30f4f235e8cc42e18463f85e12ec9b71f1865666
Drake D <drakedecker2020@gmail.com>
2025-04-16 20:31:45 -0400

    Implementation to find mean of input values

---
+++ src/ArrayOrdered.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ArrayOrdered {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        double array[] = new double[10];
+        System.out.println("Enter ten numbers for adding.");
+        for (int i = 0;i < array.length;i++){
+            array[i] = scanner.nextDouble();
+        }
+        scanner.close();
+        System.out.println("The average is:" + average(array));
+    }
+
+    public static int average(int[] array){
+        int sum = 0;
+        for (int i = 0;i < array.length;i++){
+            sum += array[i];
+        }
+        return sum/array.length;
+    }
+    public static double average(double[] array){
+        double sum = 0;
+        for (int i = 0;i < array.length;i++){
+            sum += array[i];
+        }
+        return sum/array.length;
+    }
+
+}
